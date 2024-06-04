58382fc48e8058f7b2833c07a25f827b6d31781f
Tanisha-ahsinaT <94177441+Tanisha-ahsinaT@users.noreply.github.com>
2024-06-04 21:37:48 +0530

    added logic to regenerate access token

--- average calculator/index.js
+++ average calculator/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const axios = require("axios");
 
 const app = express();
-const ACCESS_TOKEN =
+let ACCESS_TOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzE3NTEyMTE5LCJpYXQiOjE3MTc1MTE4MTksImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6ImI5YWJlNzg2LTk5MGMtNDkxYy05ZjQxLWM5M2M4MjQyYWRkYiIsInN1YiI6InRhbmlzaGExNDM1LmJlMjFAY2hpdGthcmEuZWR1LmluIn0sImNvbXBhbnlOYW1lIjoibXlNYXJ0IiwiY2xpZW50SUQiOiJiOWFiZTc4Ni05OTBjLTQ5MWMtOWY0MS1jOTNjODI0MmFkZGIiLCJjbGllbnRTZWNyZXQiOiJPTEtvcm5xSWRYREJZT3JDIiwib3duZXJOYW1lIjoiVGFuaXNoYSIsIm93bmVyRW1haWwiOiJ0YW5pc2hhMTQzNS5iZTIxQGNoaXRrYXJhLmVkdS5pbiIsInJvbGxObyI6IjIxMTA5OTE0MzUifQ.6hwLG2-qB8qAE1BuOfsyBJXbwgFbMGVlgG_fmPxDMAg";
 
 const windowSize = 10;
@@ -16,6 +16,19 @@ const Qualified_IDs = {
 let windowPrevState = [];
 let windowCurrState = [];
 
+async function updateAccessToken() {
+  const re = await axios.post("http://20.244.56.144/test/auth", {
+    companyName: "myMart",
+    clientID: "b9abe786-990c-491c-9f41-c93c8242addb",
+    clientSecret: "OLKornqIdXDBYOrC",
+    ownerName: "Tanisha",
+    ownerEmail: "tanisha1435.be21@chitkara.edu.in",
+    rollNo: "2110991435",
+  });
+  console.log(re.data.access_token);
+  ACCESS_TOKEN = re.data.access_token;
+}
+
 async function getNumbers(qualifiedId) {
   try {
     console.log(qualifiedId);
@@ -26,6 +39,7 @@ async function getNumbers(qualifiedId) {
     // const response = await axios.get(
     //   process.env.TEST_SERVER + Qualified_IDs[qualifiedId]
     // );
+    await updateAccessToken();
     const response = await axios.get(
       "http://20.244.56.144/test/" + Qualified_IDs[qualifiedId],
       {
