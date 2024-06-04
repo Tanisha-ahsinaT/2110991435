0b050b910e18c6ced6da98aa596ae4176be136e6
Tanisha-ahsinaT <94177441+Tanisha-ahsinaT@users.noreply.github.com>
2024-06-04 20:23:48 +0530

    Changes made to index.js

--- average calculator/index.js
+++ average calculator/index.js
@@ -2,22 +2,41 @@ const express = require("express");
 const axios = require("axios");
 
 const app = express();
+const ACCESS_TOKEN =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzE3NTEyMTE5LCJpYXQiOjE3MTc1MTE4MTksImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6ImI5YWJlNzg2LTk5MGMtNDkxYy05ZjQxLWM5M2M4MjQyYWRkYiIsInN1YiI6InRhbmlzaGExNDM1LmJlMjFAY2hpdGthcmEuZWR1LmluIn0sImNvbXBhbnlOYW1lIjoibXlNYXJ0IiwiY2xpZW50SUQiOiJiOWFiZTc4Ni05OTBjLTQ5MWMtOWY0MS1jOTNjODI0MmFkZGIiLCJjbGllbnRTZWNyZXQiOiJPTEtvcm5xSWRYREJZT3JDIiwib3duZXJOYW1lIjoiVGFuaXNoYSIsIm93bmVyRW1haWwiOiJ0YW5pc2hhMTQzNS5iZTIxQGNoaXRrYXJhLmVkdS5pbiIsInJvbGxObyI6IjIxMTA5OTE0MzUifQ.6hwLG2-qB8qAE1BuOfsyBJXbwgFbMGVlgG_fmPxDMAg";
 
 const windowSize = 10;
+const Qualified_IDs = {
+  p: "primes",
+  f: "fibo",
+  e: "even",
+  r: "rand",
+};
 
 let windowPrevState = [];
 let windowCurrState = [];
 
 async function getNumbers(qualifiedId) {
   try {
-    const response = await axios.get(process.env.TEST_SERVER + qualifiedId, {
-      headers: {
-        Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
-      },
-    });
-    console.log(response);
+    console.log(qualifiedId);
+    console.log(Qualified_IDs[qualifiedId]);
+    // const response = await axios.get(
+    //   process.env.TEST_SERVER + Qualified_IDs[qualifiedId]
+    // );
+    // const response = await axios.get(
+    //   process.env.TEST_SERVER + Qualified_IDs[qualifiedId]
+    // );
+    const response = await axios.get(
+      "http://20.244.56.144/test/" + Qualified_IDs[qualifiedId],
+      {
+        headers: {
+          Authorization: `Bearer ${ACCESS_TOKEN}`,
+        },
+      }
+    );
+    console.log(response.data);
     if (response.status === 200) {
-      return response.numbers;
+      return response.data.numbers;
     } else {
       return [];
     }
@@ -27,7 +46,7 @@ async function getNumbers(qualifiedId) {
 }
 
 app.get("/numbers/:qualifiedId", async (req, res) => {
-  const qualifiedId = req.params;
+  const qualifiedId = req.params.qualifiedId;
   const numbers = await getNumbers(qualifiedId);
   //   console.log(numbers);
   windowPrevState = windowCurrState;
@@ -40,7 +59,7 @@ app.get("/numbers/:qualifiedId", async (req, res) => {
       windowCurrState.push(element);
     }
   });
-  const sum = 0;
+  let sum = 0;
   windowCurrState.forEach((e) => {
     sum += e;
   });
@@ -55,4 +74,6 @@ app.get("/numbers/:qualifiedId", async (req, res) => {
   });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("listening");
+});
